9274a9380f4f2f0be5d0fafdc11226e5f3434674
Mithun@2303 <mithunkarthick1610@gmail.com>
2024-08-13 19:02:04 +0530

    ..

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,14 +1,16 @@
 import { Global, Module } from '@nestjs/common';
 import { UserService } from './user.service';
-import { UserSchemaController } from './user.controller';
+import { UserController } from './user.controller';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { TopicModule } from 'src/topic/topic.module';
 import { LeetcodeService } from './leetcode.service';
+import { AuthModule } from 'src/auth/auth.module';
+import { ProblemService } from './problem.service';
 @Global()
 @Module({
-  imports: [HttpModule, TopicModule],
-  controllers: [UserSchemaController],
-  providers: [UserService,LeetcodeService],
-  exports:[UserService]
+  imports: [HttpModule, TopicModule,AuthModule],
+  controllers: [UserController],
+  providers: [UserService, LeetcodeService,ProblemService],
+  exports: [UserService],
 })
-export class UserSchemaModule {}
+export class UserModule {}
