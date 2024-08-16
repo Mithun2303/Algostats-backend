8788e1797cbd916b6194504d3471b5e87b0db732
Jaishree_k <128470432+Jai-shree@users.noreply.github.com>
2024-08-16 15:53:37 +0530

    leaderboard

--- src/leaderboard/leaderboard.controller.ts
+++ src/leaderboard/leaderboard.controller.ts
@@ -9,17 +9,40 @@ import {
 } from '@nestjs/common';
 import { LeaderboardService } from './leaderboard.service';
 import { ApiTags } from '@nestjs/swagger';
-import { LeaderboardResponseDto } from './dto/leaderboard.dto';
+import { batchLeaderboardResponseDto, classLeaderboardResponseDto, LeaderboardResponseDto, streamLeaderboardResponseDto } from './dto/leaderboard.dto';
 
 @ApiTags('leaderboard')
 @Controller('leaderboard')
 export class LeaderboardController {
   constructor(private readonly leaderboardService: LeaderboardService) {}
 
-  @Get('class/:id')
-  async findOne(@Param('id') id: string) {
-    return (await this.leaderboardService.findClassLeaderboard(id)).map(
+  @Get()
+  async findOne() {
+    return (await this.leaderboardService.findLeaderboard()).map(
       (elt) => new LeaderboardResponseDto(elt),
+    );
+  }
+
+  @Get('class/:id')
+  async findClass(@Param('id') id: string) {
+    return (await this.leaderboardService.findClassLeaderboard(id)).map(
+      (elt) => new classLeaderboardResponseDto(elt),
+    );
+  }
+
+  @Get('stream/:id')
+  async findStream(@Param('id') id: string) {
+    return (await this.leaderboardService.findStreamLeaderboard(id))
+    .map(
+      (elt) => new streamLeaderboardResponseDto(elt),
+    );
+  }
+
+  @Get('batch/:id')
+  async findBatch(@Param('id') id: string) {
+    return (await this.leaderboardService.findBatchLeaderboard(+id))
+    .map(
+      (elt) => new batchLeaderboardResponseDto(elt),
     );
   }
 }
